8809902a4bb49133a89249ff3709f53a344d3f9c
Henry Do <661034+vitaminh@users.noreply.github.com>
2018-12-10 17:28:06 -0800

    Create depthFirstSearch.js

---
+++ easy/depthFirstSearch.js
@@ -0,0 +1,27 @@
+// Do not edit the class below except
+// for the depthFirstSearch method.
+// Feel free to add new properties
+// and methods to the class.
+class Node {
+  constructor(name) {
+    this.name = name;
+    this.children = [];
+  }
+
+  addChild(name) {
+    this.children.push(new Node(name));
+    return this;
+  }
+
+  depthFirstSearch(array) {
+    // Write your code here.
+    array.push(this.name);
+    this.children.forEach(child => {
+      child.depthFirstSearch(array);
+    })
+    return array;
+  }
+}
+
+// Do not edit the line below.
+exports.Node = Node;
