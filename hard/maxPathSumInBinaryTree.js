3dd0dac2c26f5262ae2d33a88077eee257d65380
Henry Do <661034+vitaminh@users.noreply.github.com>
2018-12-22 15:47:31 -0800

    Complete MaxPathSumInBinaryTree (Hard)

--- hard/maxPathSumInBinaryTree.js
+++ hard/maxPathSumInBinaryTree.js
@@ -8,20 +8,24 @@ const findMaxPath = tree => {
   if (!tree) return [0, 0];
   const [leftMaxSumAsBranch, leftMaxPathSum] = findMaxPath(tree.left);
   const [rightMaxSumAsBranch, rightMaxPathSum] = findMaxPath(tree.right);
-  const maxChildSumAsBranch = Math.max(
-    leftMaxSumAsBranch > rightMaxSumAsBranch
-  );
+  const maxChildSumAsBranch = Math.max(leftMaxSumAsBranch, rightMaxSumAsBranch);
 
   const { value } = tree;
   // if max child path sum is negative, we can ignore
+  // is branch greater if it includes one child or none?
   const maxSumAsBranch = Math.max(maxChildSumAsBranch + value, value);
   // get max sum of path if this node were the root
+  // if root, is it greater including two children or one/none?
   const maxSumAsRoot = Math.max(
     leftMaxSumAsBranch + value + rightMaxSumAsBranch,
     maxSumAsBranch
   );
   // what is the greatest path sum seen so far?
-  const maxPathSumValue = Math.max(maxSumAsRoot, leftMaxPathSum, rightMaxPathSum);
+  const maxPathSumValue = Math.max(
+    maxSumAsRoot,
+    leftMaxPathSum,
+    rightMaxPathSum
+  );
 
   return [maxSumAsBranch, maxPathSumValue];
 };
